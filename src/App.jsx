499e17eea0b89eacf2c4cfc830663943c261002d
elpako2354 <168143542+elpako2354@users.noreply.github.com>
2024-04-26 01:28:21 +0000

    Added the tailwind darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import './index.css'
 
 export default function App() {
   return(
-    <div className="bg-gray-400 min-h-screen">
+    <main className='relative'>
      <h1>Testing</h1>
-    </div>
+      
+    </main>
+   
   )
 }
