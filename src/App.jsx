021ad31e11df643d636e0599f97ce011e5cb6ed8
elpako2354 <168143542+elpako2354@users.noreply.github.com>
2024-04-26 19:19:39 +0000

    Added the first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './index.css'
 import bgImg from './assets/images/background.jpg' 
-    
+import Title from './components/Title'
+
+
 export default function App() {
   return(
 <>
     <div className='relative bg-cover bg-center h-screen'>
       <img src={bgImg} className='absolute inset-0 w-full h-full object-cover'/>
     </div>
-  <div className='absolute inset-0 flex justify-content items-center'>
-    <h1 className='text-center text-4xl text-white font-bold'>Webundle</h1>
-    <p className='mt-2 text-white'>More content here</p>
-  </div>
+    <Title />
 </>
     
   )
---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+export default function Title() {
+  return(
+    <div className='absolute inset-0 flex justify-content items-center'>
+      <h1 className='text-center text-4xl text-white font-bold'>Webundle</h1>
+    </div>
+  )
+}
