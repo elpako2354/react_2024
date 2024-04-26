ef83fd7129db344f1f2c4973affd37d74ed9011a
elpako2354 <168143542+elpako2354@users.noreply.github.com>
2024-04-26 20:56:47 +0000

    Footer on-going

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import './index.css'
 import bgImg from './assets/images/background.jpg' 
 import Title from './components/HomePage/Title'
 import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 
 export default function App() {
   return(
 <>
-    <div className='relative bg-cover bg-center h-screen'>
-      <img src={bgImg} className='absolute inset-0 w-full h-full object-cover'/>
+    <div className='relative bg-cover bg-center h-screen bg-black'>
+      <img src={bgImg} className='absolute inset-0 w-full h-full object-cover bg-black'/>
     </div>
-    <Title />
    <Navbar />
+   <Title />
+   <Footer />
 </>
     
   )
