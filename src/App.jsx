3a69a1825d7c702d26450f2904f2a131408da474
elpako2354 <168143542+elpako2354@users.noreply.github.com>
2024-04-26 19:10:52 +0000

    Added text over bg

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ export default function App() {
     <div className='relative bg-cover bg-center h-screen'>
       <img src={bgImg} className='absolute inset-0 w-full h-full object-cover'/>
     </div>
+  <div className='absolute inset-0 flex justify-content items-center'>
+    <h1 className='text-center text-4xl text-white font-bold'>Webundle</h1>
+    <p className='mt-2 text-white'>More content here</p>
+  </div>
 </>
     
   )
