cc6096690471584f1501988c76e6f4cbd241e3b6
elpako2354 <168143542+elpako2354@users.noreply.github.com>
2024-04-26 16:07:02 +0000

    _refresh background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import './index.css'
-import background from './assets/images/background.jpg' 
+import backgroundImage from './assets/images/background.jpg' 
 
 export default function App() {
   return(
-
- <section>
-   <div>
-      <img src={background} alt=""/>
-   </div>
-   <div>
-      <h1>Hello, world!</h1>
-     <p>Some text here</p>
-   </div>
+    <div>
+      <img src={backgroundImage} alt="background" />
+    <h1>23</h1>
+    </div>
   
- 
- </section>
-   
   )
 }
